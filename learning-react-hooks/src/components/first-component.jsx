c79fe5e6f5b334c75dd1ee0a1ef9d4997446260c
Prashika <prashikasonule@gmail.com>
2021-09-08 19:39:41 +0530

    Created new component

---
+++ learning-react-hooks/src/components/first-component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function FirstComponent() {
+    return (
+        <div>
+            <header>
+                {'Hello Welcome in first component'}
+            </header>
+        </div>
+    );
+}
+
+export default FirstComponent;
